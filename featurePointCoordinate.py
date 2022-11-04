1283f305eb88c62cbe01cbb7ad632c9c14a49326
Takuchan <takuchanapp@gmail.com>
2022-11-04 18:54:36 +0900

    類似点座標xyを前画像と後画像ごとにndarrayに変換した

--- featurePointCoordinate.py
+++ featurePointCoordinate.py
@@ -1,6 +1,12 @@
 from nis import match
+from tkinter import N
 import cv2
 import numpy as np
+
+
+# initialize variable
+image1list = []
+image2list = []
 
 # load_image
 load_image1 = cv2.imread("../sozai/1.jpg")
@@ -23,14 +29,19 @@ bf = cv2.BFMatcher()
 matches = bf.match(des1,des2)
 matches = sorted(matches,key = lambda x:x.distance)
 for x in range(len(matches[:20])): 
-    print(type(kp1[matches[x].queryIdx].pt))
+    print(kp1[matches[x].queryIdx].pt)
     print(kp2[matches[x].trainIdx].pt)
+    image1list.append(kp1[matches[x].queryIdx].pt)
+    image2list.append(kp2[matches[x].trainIdx].pt)
     print("---------------------------------------------")
 
 
 # [:<this>]this argument is feature point figure
 img3 = cv2.drawMatches(gray_image1,kp1,gray_image2,kp2,matches[:20],None,flags=2)
-
+beforeCameraResult = np.array(image1list,dtype=float)
+afterCameraResult = np.array(image2list,dtype=float)
+print("moto-gazo",beforeCameraResult)
+print("ato-gazo",afterCameraResult)
 cv2.imshow("image2",gray_image2)
 cv2.imshow("show",img3)
 cv2.waitKey(0)
